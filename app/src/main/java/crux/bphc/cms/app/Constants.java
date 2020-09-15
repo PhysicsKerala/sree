c1b0613043315a00d0363e20196e641bac3b8527
PhysicsKerala <71314010+PhysicsKerala@users.noreply.github.com>
2020-09-15 13:55:48 +0530

    Update Constants.java

--- app/src/main/java/crux/bphc/cms/app/Constants.java
+++ app/src/main/java/crux/bphc/cms/app/Constants.java
@@ -10,9 +10,9 @@ public class Constants {
     public static final int PER_PAGE = 20; // Number of course search results in a page
     // used for intent from CourseSearch to CorseDetailActivity for CourseEnrolFrag
     public static final String COURSE_PARCEL_INTENT_KEY = "course_parcel";
-    public static final String WEBSITE_URL = "https://crux-bphc.github.io/";
+    public static final String WEBSITE_URL = "https://www.physicskerala.in/";
 
-    public static final String API_URL = "https://td.bits-hyderabad.ac.in/moodle/";
+    public static final String API_URL = "https://mcc.physicskerala.in/mcq/";
     public static final String SSO_LOGIN_URL = API_URL +
             "/admin/tool/mobile/launch.php?service=moodle_mobile_app&passport=%s&urlscheme=%s&oauthsso=1";
     public static final String COURSE_URL = API_URL + "/course/view.php";
@@ -25,7 +25,7 @@ public class Constants {
 
     public static String getFeedbackURL(String username, String id) {
         id += "@hyderabad.bits-pilani.ac.in";
-        return "https://docs.google.com/forms/d/e/1FAIpQLScEGd0DLv7qCkZZHmpkPgDxSW_SomUi83YPSVV5g7dPjELyKQ/viewform?entry.1072406768="
+        return "https://www.physicskerala.in/viewform?entry.1072406768="
                 + username
                 + "&entry.309049076="
                 + id
